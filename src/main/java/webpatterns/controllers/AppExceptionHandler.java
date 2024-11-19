443259ebcc5dacc52ce76f92340f69f343f8b231
sophie <d00259237@student.dkit.ie>
2024-11-19 12:19:24 +0000

    Implemented AppExceptionHandler to handle exception in the application

--- src/main/java/webpatterns/controllers/AppExceptionHandler.java
+++ src/main/java/webpatterns/controllers/AppExceptionHandler.java
@@ -1,4 +1,22 @@
 package webpatterns.controllers;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class AppExceptionHandler {
+
+    @ExceptionHandler(value = NullPointerException.class)
+    public String nullPointerHandler(Model model) {
+        model.addAttribute("err", "NullPointerException");
+        return "error";
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public String allOtherExceptionHandler(Model model, Exception ex) {
+        model.addAttribute("errType", ex.getClass());
+        model.addAttribute("errMsg", ex.getMessage());
+        return "error";
+    }
 }
