9deef6e4d26b3b497c8295b6affe4a1f7c20c8d3
sophie <d00259237@student.dkit.ie>
2024-11-19 12:17:53 +0000

    Implemented ErrorHandler to handle exceptions

--- src/main/java/webpatterns/controllers/ErrorHandler.java
+++ src/main/java/webpatterns/controllers/ErrorHandler.java
@@ -1,4 +1,15 @@
 package webpatterns.controllers;
 
-public class ErrorHandler {
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ErrorHandler implements ErrorController {
+    private final static String PATH = "/error";
+
+    @RequestMapping(PATH)
+    @ResponseBody
+    public String getErrorPath(){ return "No Mapping Found;"}
 }
