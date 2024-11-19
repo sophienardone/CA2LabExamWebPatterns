1c9aa3fe0f2eb81f38d5e32a2b9c1d0bdbd5c504
sophie <d00259237@student.dkit.ie>
2024-11-19 10:27:35 +0000

    Created appropriate packages and classes for BlogSystem

---
+++ src/main/java/webpatterns/main/Ca2LabExamApplication.java
@@ -0,0 +1,13 @@
+package webpatterns.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ca2LabExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ca2LabExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ca2labexam/Ca2LabExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ca2labexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ca2LabExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
