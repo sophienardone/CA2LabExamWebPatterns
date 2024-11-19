baefd9974b89034f4987d2aacb16996425eeb3bc
sophie <d00259237@student.dkit.ie>
2024-11-19 12:14:20 +0000

    Implemented IndexController for navigation

--- src/main/java/webpatterns/controllers/IndexController.java
+++ src/main/java/webpatterns/controllers/IndexController.java
@@ -1,4 +1,21 @@
 package webpatterns.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+    @GetMapping
+    public String home(){ return "index";}
+    @GetMapping("/messages_index")
+    public String messageIndex(){ return "messages_index;"}
+    @GetMapping("/user_index")
+    public String userIndex(){return "user_index";}
+    @GetMapping("friends_index")
+    public String friendsIndex(){return "friends_index";}
+    @GetMapping("blogentries_index")
+    public String blogentriesIndex(){return "blogentriesIndex";}
+
+
 }
